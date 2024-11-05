3b3cbfd132f29adea7e3d1a61f7bfde7c0868389
Remedeae <sanna.m.blomdahl@gmail.com>
2024-11-05 14:12:07 +0100

    added error messages and functionality to prevent taking out moe money then what's on the account

--- index.js
+++ index.js
@@ -3,36 +3,53 @@ const account = {
     balance: 9999,
     loginName: null,
     login: false,
+    accountError () {
+        console.log("Insufficient funds.");
+        const errorMessage = prompt ("Would you still like to make a withdrawal? Y/N");
+        switch (errorMessage) {
+            case "Y":
+            case "y":
+                this.withdrawal();
+                break;
+            case "N":
+            case "n":
+        }
+    },
     getBalance() {
         console.log(`You have ${this.balance}SEK on your account.`);
     },
     deposit() {
         this.balance += parseFloat(
-            prompt("How much do you wanna deposit?"));
+            prompt("How much do you want to deposit?"));
+        this.getBalance ();
     },
     withdrawal() {
-        this.balance -= parseFloat(
+        let withdraw = parseFloat(
             prompt(
-            "How much do you wanna withdraw?"));
+            "How much do you want to withdraw?"));
+        if (this.balance > withdraw) {
+            this.balance -= withdraw;
+            this.getBalance ();
+            return this.balance;
+        }
+        else {
+            this.accountError ();
+        }
     },
     getAccountName () {
         console.log(this.accountName);
     },
-    accountError () {
-        if (this.balance < 0) {
-            this.balance = 0;
-        }
-/*         return this.balance; */
-    },
     //In the beginning I started making the accountError as the loginPrompt, but realised that was 
     //probably not what this was meant to be. But I kept it since I thought it was neat :)
     loginPrompt () {
-        while (this.loginName !== this.accountName) {
-                this.loginName = prompt("Enter account name.");
-                console.log("Unknown account name, please try again.")
+        this.loginName = prompt("Enter account name.");
+        if (this.loginName !== this.accountName) {
+            this.loginName = prompt("Unknown account name, please try again.");
+        }
+        else {
+            console.log(`Welcome ${this.accountName}!`);
+            return this.login = true;
             }
-        console.log(`Welcome ${this.accountName}!`);
-        return this.login = true;
         },
     exitAccount() {
         this.login = false;
@@ -57,8 +74,6 @@ function atm () {
             break;
         case 3:
             account.withdrawal ();
-            account.accountError ();
-            account.getBalance ();
             break;
         case 4:
             account.getAccountName ();
